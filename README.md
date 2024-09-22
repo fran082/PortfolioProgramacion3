63fbcdfe5bd9d34b5434bddc30dc175b380b1f87
Francisco Nahuel Fonseca <128767970+fran082@users.noreply.github.com>
2024-09-22 18:13:48 -0300

    Primera subida
    Aclaracion fue subida todas las carpetas
    -git
    -source
    -assets
    index.html
    README.md
    
    Nose si hay que subir la carpeta'git'

---
+++ README.md
@@ -0,0 +1,2 @@
+# PRIMERA_PRUEBA_PORTFOLIO
+PORTFOLIO SUBIDO A GIT HUB PAGES. Primera subida. no tiene menu. no tiene formulario.
---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+
+
+  
+/* function sendEmail() {
+    // Obtiene los valores del formulario
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var email = document.getElementById("email").value;
+    var mensaje = document.getElementById("mensaje").value;
+  
+    // Crea el cuerpo del correo en formato texto
+    var body = `Nombre: ${nombre}%0D%0AApellido: ${apellido}%0D%0AEmail: ${email}%0D%0A%0D%0AMensaje:%0D%0A${mensaje}`;
+  
+    // Abre la aplicación de correo con el destinatario y el cuerpo del mensaje
+    window.location.href = `mailto:francisco.n.fonseca.tech@outlook.com?subject=Mensaje de contacto&body=${body}`;
+  } */
+
+
+
+    /*
+  function sendEmail() {
+    // Previene el envío automático del formulario
+    event.preventDefault();
+  
+    // Obtiene los valores del formulario
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var email = document.getElementById("email").value;
+    var mensaje = document.getElementById("mensaje").value;
+    var asunto = document.getElementById("asunto").value;
+  
+    // Crea el cuerpo del correo en formato texto con saltos de línea
+    var body = `Apellido: ${apellido}%0D%0ANombre: ${nombre}%0D%0AAsunto: ${asunto}%0D%0AMensaje:%0D%0A${mensaje}`;
+  
+    // Abre la aplicación de correo con el destinatario, asunto y cuerpo del mensaje
+    window.location.href = `mailto:francisco.n.fonseca.tech@outlook.com?subject=Mensaje de contacto&body=${body}`;
+  }
+
+  */
